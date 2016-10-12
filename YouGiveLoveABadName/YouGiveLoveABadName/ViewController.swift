e10b6f3fde5dfa5e9bdde4fbdeab232f9a822083
joelconnects <joelconnects@gmail.com>
2016-10-12 17:05:08 -0400

    updated for Swift 3, added solution updated README

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -10,12 +10,96 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contractedHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var expandedHeightConstraint: NSLayoutConstraint!
+    @IBOutlet var imageTapGesture: UITapGestureRecognizer!
+    
+    var isExpanded: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
-    @IBAction func expandButtonTapped(sender: AnyObject) {
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        
+        !isExpanded ? expandImageView() : contractImageView()
+
+    }
+    
+    func expandImageView() {
+        
+        imageTapGesture.isEnabled = false
+        
+        UIView.animateKeyframes(withDuration: 0.5, delay: 0.0, options: [], animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.2) {
+                
+                self.contractedHeightConstraint.constant = -75
+                self.view.layoutIfNeeded()
+                
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.4) {
+                
+                self.contractedHeightConstraint.isActive = false
+                self.expandedHeightConstraint.isActive = true
+                self.expandedHeightConstraint.constant = 200
+                self.view.layoutIfNeeded()
+                
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.6, relativeDuration: 0.4) {
+                
+                self.expandedHeightConstraint.constant = 0
+                self.view.layoutIfNeeded()
+                
+            }
+            
+        }) { _ in
+            
+            self.isExpanded = true
+            self.imageTapGesture.isEnabled = true
+            
+        }
+        
+    }
+    
+    func contractImageView() {
+        
+        imageTapGesture.isEnabled = false
+        
+        UIView.animateKeyframes(withDuration: 0.5, delay: 0.0, options: [], animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.2) {
+                
+                self.expandedHeightConstraint.constant = 200
+                self.view.layoutIfNeeded()
+                
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.4) {
+                
+                self.expandedHeightConstraint.isActive = false
+                self.contractedHeightConstraint.isActive = true
+                self.contractedHeightConstraint.constant = -75
+                self.view.layoutIfNeeded()
+                
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.6, relativeDuration: 0.4) {
+                
+                self.contractedHeightConstraint.constant = 0
+                self.view.layoutIfNeeded()
+                
+            }
+            
+        }) { _ in
+            
+            self.isExpanded = false
+            self.imageTapGesture.isEnabled = true
+            
+        }
         
     }
 
