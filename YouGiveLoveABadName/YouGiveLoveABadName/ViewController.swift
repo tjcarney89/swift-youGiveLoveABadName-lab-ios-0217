deff74954f5e21c4de5ab3b0e8f811f095cdc820
TJ Carney <tjcarney@TJs-MacBook-Air.local>
2017-02-28 12:03:28 -0500

    Completed Lab and Successfully Ran App

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var bonJoviHeight: NSLayoutConstraint!
+    
+    
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +23,58 @@ class ViewController: UIViewController {
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        
+        print(bonJoviHeight.constant)
+        print(UIScreen.main.bounds.height)
+        
+        if bonJoviHeight.constant == 0 {
+            UIView.animateKeyframes(withDuration: 1.0, delay: 0.0, options: [], animations: {
+                
+                
+                UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.1, animations: {
+                    self.bonJoviHeight.constant = self.bonJoviHeight.constant - 10
+                    self.view.layoutIfNeeded()
+                    print("#1 CALLED")
+                })
+                
+                UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.2, animations: {
+                    self.bonJoviHeight.constant = self.view.frame.height + 200
+                    self.view.layoutIfNeeded()
+                    print("#2 CALLED")
+                })
+                
+                UIView.addKeyframe(withRelativeStartTime: 0.3, relativeDuration: 0.5, animations: {
+                    self.bonJoviHeight.constant = self.view.frame.height - (self.view.frame.height/15)
+                    self.view.layoutIfNeeded()
+                    print("#3 CALLED")
+                })
+            
+            }, completion: nil)
+            
+        } else if bonJoviHeight.constant == self.view.frame.height - (self.view.frame.height/15) {
+            UIView.animateKeyframes(withDuration: 1.0, delay: 0.0, options: [], animations: {
+                
+                UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.1, animations: {
+                    self.bonJoviHeight.constant = self.view.frame.height + 200
+                    self.view.layoutIfNeeded()
+                    print("#4 CALLED")
+                })
+                
+                UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.2, animations: {
+                    self.bonJoviHeight.constant = -20
+                    self.view.layoutIfNeeded()
+                    print("#5 CALLED")
+                })
+                
+                UIView.addKeyframe(withRelativeStartTime: 0.3, relativeDuration: 0.2, animations: {
+                    self.bonJoviHeight.constant = 0
+                    self.view.layoutIfNeeded()
+                    print("#6 CALLED")
+                })
+                
+                
+            }, completion: nil)
+        }
         
 
     }
